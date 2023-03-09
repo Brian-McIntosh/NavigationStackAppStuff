1aea68a5b4336b746c89bf17951bca3bb1758e60
Brian McIntosh <brian@mixinteractive.com>
2023-03-09 12:23:34 -0500

    Initial Commit

---
+++ NavigationStackAppStuff/NavigationStackAppStuffApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationStackAppStuffApp.swift
+//  NavigationStackAppStuff
+//
+//  Created by Brian McIntosh on 3/9/23.
+//
+
+import SwiftUI
+
+@main
+struct NavigationStackAppStuffApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
